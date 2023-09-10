eaea4e7fb174005526ed448761af70562f8fd9b6
Abbilville <abbilville@gmail.com>
2023-09-10 16:45:41 +0700

    Create testing

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from .models import Item
 
-# Create your tests here.
+class ItemTestCase(TestCase):
+    
+    def setUp(self):
+        self.item = Item.objects.create(
+            name = "Milk",
+            amount = 20,
+            description = "Fresh milk from cows",
+            price = 5000,
+            category = "diary",
+        )
+
+    def test_item(self):
+        item_from_databases = Item.objects.get(id = self.item.id)
+
+        self.assertEqual(item_from_databases.name, "Milk")
+        self.assertEqual(item_from_databases.amount, 20)
+        self.assertEqual(item_from_databases.description, "Fresh milk from cows")
+        self.assertEqual(item_from_databases.price, 5000)
+        self.assertEqual(item_from_databases.category, "diary")
