9cb0d1af52da21f7cd1ed831b123aa2459a84fa5
Abbilville <abbilville@gmail.com>
2023-09-10 16:45:29 +0700

    add id variables for testing

--- main/models.py
+++ main/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+import uuid
 
 class Item(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length = 255)
     amount = models.IntegerField()
     description = models.TextField()
     price = models.IntegerField()
-    power = models.IntegerField()
-    category = models.CharField(max_length=255)
+    category = models.CharField(max_length = 255)
+    id = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
